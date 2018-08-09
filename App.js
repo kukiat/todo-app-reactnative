a05354b19295842846d42f845abb745e19a850c7
kukiat <kukiat1996@gmail.com>
2018-08-10 00:31:51 +0700

    Add simple state todos

--- App.js
+++ App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [
+        { id: 0, title: 'todstodstods', description: 'Prebid Adds Support for Mobile App Header', status: false },
+        { id: 1, title: 'Lorem Ipsum', description: 'Prebid Adds Support for Mobile App Header', status: false },
+        { id: 2, title: 'todstLorem odstods', description: 'Prebid Adds Support for Mobile App Header', status: false },
+      ]
+    }
+  }
+
+  
   render() {
+    const { todos } = this.state
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}> wdwdwddw React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        { todos.map(todo => (
+          <Text key={todo.id} style={styles.welcome}>{todo.title}</Text>
+        ))}
       </View>
     );
   }
@@ -14,10 +28,10 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5F5F5',
   },
   welcome: {
     fontSize: 20,
