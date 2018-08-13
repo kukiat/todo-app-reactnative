de5624270d4b3481143f2788b01181bd67a0f338
kukiat <kukiat1996@gmail.com>
2018-08-14 02:43:46 +0700

    Restructor Folder

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createStackNavigator } from 'react-navigation'
-import TodoApp from './TodoApp'
-import TodoCreate from './TodoCreate'
-import TodoDetail from './TodoDetail'
-import TodoEdit from './TodoEdit'
+import TodoApp from './components/TodoApp'
+import TodoCreate from './components/TodoCreate'
+import TodoDetail from './components/TodoDetail'
+import TodoEdit from './components/TodoEdit'
 
 export default createStackNavigator({
   TodoApp: { screen: TodoApp },
